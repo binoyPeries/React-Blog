16e7ad1bb4f40bb8d6c9171edb0c1e88587422d0
binoy <binoy.18@cse.mrt.ac.lk>
2021-03-16 08:40:28 +0530

    loading added

--- src/blogList.js
+++ src/blogList.js
@@ -1,14 +1,16 @@
 const BlogList = (props) => {
     const blogs = props.blogs;
-    console.log(props);
+    const pgtitle = props.title
+    const handleDelete = props.handleDelete
     return (  
+       
         <div className="blog-list">
+         <h2>{pgtitle}</h2>
             {blogs.map((blog)=>(
                 <div className="blog-preview" key={blog.id}>
                 <h2>{blog.title}</h2>
                 <h4>{blog.author}</h4>
-
-
+            
                 </div>
 
             ))}
--- src/home.js
+++ src/home.js
@@ -1,23 +1,54 @@
-import {useState} from 'react';
+import {useState , useEffect} from 'react';
 import BlogList from './blogList';
 const Home = () => {
     // const [name,setName]=useState('binoy')
     // first para is the variable name
     //second para is the func that can be used to change the variable
 
-    const [blogs, setBlogs] = useState([
-        {title:'React vs Angular', author:"binoy peries" , body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.', id:1},
-        {title:'Node.js', author:"John peries" , body: ' Id obcaecati eum, aut repellat, ducimus sapiente error eveniet velit .', id:2},
-        {title:'Flutter', author:"Yurantha peries" , body: ' in consectetur doloribus rem iste aperiam accusantium,aliquam perferendis hic exercitationem cupiditate..', id:3}
+    const [blogs, setBlogs] = useState(null);
+    const [isloading, setISloading] = useState(true);
+    const [err, setErr] = useState(null);
 
-    ]);
+    //dummy func to mimic dependencies
+    // const [name,setName] = useState('mario');
+
+    //this we run everytime the page gets re rendered
+    //use effect dependencies allows\ us to define when to run the useEffect //second parameter
+    useEffect(()=>{
+        fetch('http://localhost:8000/blog')
+        .then(res =>{
+            if(res.ok){
+                return res.json()
+            }
+            else{
+                throw Error('Could not fetch data!!!')
+            }
+            
+        })
+        .then( data=>{
+            setBlogs(data);      
+            setISloading(false) ; 
+            setErr(null);
+        })
+        .catch((e)=>{
+            setBlogs(null);
+            setErr(e.message);
+            setISloading(false);
+        })
+        console.log('use effect');
+
+    },[]);
+    //whatever inside the array, if these states changes the useEffect re runs
+    const handleDelete =(id)=>{
+        const tempblog = blogs.filter((b)=>b.id !==id);
+        setBlogs(tempblog);
+    }
     
     // const handleClick=()=>{
     //     setName('peries')
     // }
     return (  
         <div className="home">
-   
             {/* <h2>Welcome to Homepage</h2> */}
 
             {/* <button onClick= {()=>handleClick("same")}> Click me</button> */}
@@ -27,7 +58,12 @@ const Home = () => {
             {/* key (Id) property is a must in react */}
             {/* pass data from parent to child we use prop */}
 
-            <BlogList blogs={blogs}/>
+            {err && <div>{err} </div> }
+            {isloading && <div>Data is Loading.... </div>}
+            {blogs && <BlogList blogs={blogs}  title ="All Blogs" />}
+         
+            <br/>
+            {/* <BlogList blogs={blogs.filter((blog)=> blog.author =='binoy')}  title ="Binoy's Blogs"/> */}
 
     
 
