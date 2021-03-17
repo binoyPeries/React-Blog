f721cf2b9b928ce988b0cf9dd96702586d839b99
binoy <binoy.18@cse.mrt.ac.lk>
2021-03-17 09:10:55 +0530

    routing added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import Navbar from './navbar';
 import Home from './home';
-function App() {
-  const titleName = "welcome to homepage" 
-  //we cant output objects directly
-  return (
-    <div className="App">
-    <Navbar/>
-    <div className="content">
-        <Home/>
+import CreateBlog from './createBlog';
+import {BrowserRouter as Router, Route,Switch} from 'react-router-dom';
+import BlogDetails from './blogDetails';
 
-    </div>
+//cover the app with router so thta it can be used anywahere
+function App() {
+
+ 
+  return (
+    <Router>
+    <div className="App">
+      <Navbar/>
+      <div className="content">
+      {/* makes sure that only one route is shown at a given time */}
+      <Switch>
+      {/* place all the routes here */}
+        <Route exact path='/'>
+            <Home/>
+        </Route>
+
+        <Route exact path='/create'>
+            <CreateBlog/>
+        </Route>
+
+        <Route exact path='/blog/:id'>
+            <BlogDetails/>
+        </Route>
+
+      </Switch>
+
+
+      </div>
     
     </div>
+    </Router>
   );
 }
 
